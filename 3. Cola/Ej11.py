9d7c18219cbdb84b5295b24d1830ac443cbfcd92
Mari Beltrami <mariabelt13@gmail.com>
2024-09-23 18:16:45 -0300

    commit

---
+++ 3. Cola/Ej11.py
@@ -0,0 +1,7 @@
+# Dada una cola con personajes de la saga Star Wars, de los cuales se conoce su nombre y planeta
+# de origen. Desarrollar las funciones necesarias para resolver las siguientes actividades:
+#     a. mostrar los personajes del planeta Alderaan, Endor y Tatooine
+#     b. indicar el plantea natal de Luke Skywalker y Han Solo
+#     c. insertar un nuevo personaje antes del maestro Yoda
+#     d. eliminar el personaje ubicado después de Jar Jar Binks
+
--- 4. Lista/lista.py
+++ 4. Lista/lista.py
@@ -1,13 +1,57 @@
+class Lista:
+    
+    def __init__(self):
+        self.inicio = None
+        self.tamanio = 0    
+    
+# Devuelve un puntero que apunta al nodo que contiene un elemento que
+#coincida con la clave –el primero que encuentra–, si devuelve None significa que no se encontró
+#la clave en la lista
+    def search(lista, criterio, valor):
+        for indice, elemento in enumerate(lista):
+            if elemento[criterio] == valor:            
+                return indice
+    
+
+#Elimina y devuelve de la lista si encuentra un elemento que coincida con
+#la clave dada –el primero que encuentre–, si devuelve None significa que no se encontró la clave
+#en la lista, y por ende no se elimina ningún elemento
+    def remove(lista, criterio, valor):
+            indice = search(lista, criterio, valor)
+            if indice is not None:
+                return lista.pop(indice)
+
+    def ordenar(lista, criterio, reverse = False): #Defino esta función para no tener que definir más funciones by_criterio
+        lista.sort(key = lambda item: item[criterio], reverse = reverse)
+
+#Realiza un recorrido de la lista mostrando la información de los elementos al-
+#macenado en la lista.
+    def barrido_continuo(lista):
+        cadena = ""
+        for indice, elemento in enumerate(lista):
+            cadena += elemento + (", " if indice < len(lista) - 2 else " y " if indice == len(lista) - 2 else "")
+            return cadena
 
 
+
+
+
+
+
+"""
 # lista_de_elementos = [1, 20, 5, 67, 3, 4, -1]
 
-# #! insertar con posicion
+#! insertar con posicion
+#! insert(lista,elemento). Agrega el elemento a la lista de manera que el mismo quede ordenado.
 # lista_de_elementos.insert(2, 99)
+
 # #! insertar sin posicion
 # lista_de_elementos.append(123)
 
-# #! eliminar un elemento de la lista (suponiendo valores unicos y dato simple)
+#! eliminar un elemento de la lista (suponiendo valores unicos y dato simple)
+#! eliminar(lista, clave). Elimina y devuelve de la lista si encuentra un elemento que coincida con
+#!la clave dada –el primero que encuentre–, si devuelve None significa que no se encontró la clave
+#!en la lista, y por ende no se elimina ningún elemento;
 # try:
 #     lista_de_elementos.remove(101)
 # except ValueError:
@@ -18,7 +62,7 @@
 # else:
 #     print('el elemento a eliminar no esta en la lista')
 
-# #! tamanio
+#! tamanio. tamaño(lista). Devuelve la cantidad de elementos en la lista
 # print(len(lista_de_elementos))
 
 # #! ordenar elementos simples
@@ -49,8 +93,10 @@
 # ]
 
 # nuevo_personaje = {'nombre': 'Boba Fett', 'especie': 'Humano', 'altura': 178}
+
 #! insertar con posicion
 # personajes_star_wars.insert(8, nuevo_personaje)
+
 #! insertar sin posicion
 # nuevo_personaje_2 = {'nombre': 'Mace Windu', 'especie': 'Humano', 'altura': 190}
 # personajes_star_wars.append(nuevo_personaje_2)
@@ -110,4 +156,6 @@ def show_list_list(title, subtitle, list_values):
         print(f"    {subtitle}")
         for second_index, second_element in enumerate(elemento['sublist']):
             print('    ', second_index, second_element)
-    print()
+print()
+
+"""
